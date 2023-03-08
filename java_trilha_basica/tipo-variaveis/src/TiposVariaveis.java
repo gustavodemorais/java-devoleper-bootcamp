a993ae39362a1db0de1714f7dddc6f0b5d0190d0
Gustavo Morais <125063612+gustavodemorais@users.noreply.github.com>
2023-03-08 13:20:29 -0300

    atualização

--- java_trilha_basica/tipo-variaveis/src/TiposVariaveis.java
+++ java_trilha_basica/tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+        
+        String meuNome = "Gustavo Morais";
+        
         System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
